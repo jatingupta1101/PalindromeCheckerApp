2ccd9b3e934dcc6502eff4eac4aafddec0caa4d0
jatingupta1101 <1101jatingupta@gmail.com>
2026-02-20 10:46:53 +0530

    UC: Palindrome check using deque

---
+++ src/UseCase7PalindromeCheckerApp.java
@@ -0,0 +1,34 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class UseCase7PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String word = "madam";   // Hardcoded string
+
+        Deque<Character> deque = new LinkedList<>();
+
+        // Insert characters into deque
+        for (int i = 0; i < word.length(); i++) {
+            deque.addLast(word.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+
+        // Compare front and rear
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Result
+        if (isPalindrome) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
+    }
+}
