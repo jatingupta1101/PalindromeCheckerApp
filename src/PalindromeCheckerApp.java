053c70b2d65b709a58f3c6d0c0dcc695b86c3679
jatingupta1101 <yl9442@srmist.edu.in>
2026-02-27 09:23:04 +0530

    UC8: Linked List Based Palindrome Checker - Correct Author

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,70 @@
 public class PalindromeCheckerApp {
+
+    static class Node {
+        char data;
+        Node next;
+
+        Node(char data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static boolean isPalindromeUsingLinkedList(String str) {
+
+        if (str == null || str.length() == 0)
+            return true;
+
+        Node head = new Node(str.charAt(0));
+        Node current = head;
+
+        for (int i = 1; i < str.length(); i++) {
+            current.next = new Node(str.charAt(i));
+            current = current.next;
+        }
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node prev = null;
+        Node nextNode;
+
+        while (slow != null) {
+            nextNode = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = nextNode;
+        }
+
+        Node firstHalf = head;
+        Node secondHalf = prev;
+
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data)
+                return false;
+
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
+
         System.out.println("Welcome to Palindrome Checker App");
+
+        String input = "madam";
+
+        if (isPalindromeUsingLinkedList(input)) {
+            System.out.println("UC8: " + input + " is a Palindrome");
+        } else {
+            System.out.println("UC8: " + input + " is NOT a Palindrome");
+        }
     }
 }
